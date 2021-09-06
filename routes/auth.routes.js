2bb770f2fc87e452b2a9946071cd22453274607f
ivil2021 <89508514326@mail.ru>
2021-09-06 23:37:06 +0300

    added hashed password but there is a mistake

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 //подключаем роутер из экспресса
 const { Router } = require('express');
+// bcrypt позволяет хэшировать пароли и сравнивать их
+// const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const router = Router();
 
@@ -11,13 +13,21 @@ router.post('/register', async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        const candidate = await User.findOne({ email });
+        const candidate = await User.findOne({ email }); // { email: email }
 
         if (candidate) {
             return res
                 .status(400)
                 .json({ message: 'Такой пользователь уже существует' });
         }
+
+        // если пользователя с таким имэйлом нет, регистрируем, хэшируем его пароль
+        const hashedPassword = await bcrypt.hash(password, 12);
+        const user = new User({ email, password: hashedPassword });
+
+        await user.save();
+
+        res.status(201).json({ message: 'Пользователь создан' });
     } catch (e) {
         res.status(500).json({
             message: 'Что-то пошло не так, попробуйте снова',

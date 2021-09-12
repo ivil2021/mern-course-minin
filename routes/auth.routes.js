a1ac386ba078866a7aa42458bd4be75a5f0c3d2a
ivil2021 <89508514326@mail.ru>
2021-09-12 22:26:43 +0300

    the register method works

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,8 @@
 //подключаем роутер из экспресса
 const { Router } = require('express');
 // bcrypt позволяет хэшировать пароли и сравнивать их
-// const bcrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 const router = Router();
 
@@ -9,31 +10,49 @@ const router = Router();
 // префикс из app.use('/api/auth', require('./routes/auth.routes'));
 // /api/auth будет конкатенироваться с /register
 // /api/auth/register
-router.post('/register', async (req, res) => {
-    try {
-        const { email, password } = req.body;
+router.post(
+    '/register',
+    [
+        check('email', 'Некорректный email').isEmail(),
+        check('password', 'Минимальная длина пароля 6 символов').isLength({
+            min: 6,
+        }),
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
 
-        const candidate = await User.findOne({ email }); // { email: email }
+            if (!errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Некорректные данные при регистрации',
+                });
+            }
 
-        if (candidate) {
-            return res
-                .status(400)
-                .json({ message: 'Такой пользователь уже существует' });
+            const { email, password } = req.body;
+
+            const candidate = await User.findOne({ email }); // { email: email }
+
+            if (candidate) {
+                return res.status(400).json({
+                    message: 'Такой пользователь уже существует',
+                });
+            }
+
+            // если пользователя с таким имэйлом нет, регистрируем, хэшируем его пароль
+            const hashedPassword = await bcrypt.hash(password, 12);
+            const user = new User({ email, password: hashedPassword });
+
+            await user.save();
+
+            res.status(201).json({ message: 'Пользователь создан' });
+        } catch (e) {
+            res.status(500).json({
+                message: 'Что-то пошло не так, попробуйте снова',
+            });
         }
-
-        // если пользователя с таким имэйлом нет, регистрируем, хэшируем его пароль
-        const hashedPassword = await bcrypt.hash(password, 12);
-        const user = new User({ email, password: hashedPassword });
-
-        await user.save();
-
-        res.status(201).json({ message: 'Пользователь создан' });
-    } catch (e) {
-        res.status(500).json({
-            message: 'Что-то пошло не так, попробуйте снова',
-        });
     }
-});
+);
 
 // /api/auth/login
 router.post('/login', async (req, res) => {});

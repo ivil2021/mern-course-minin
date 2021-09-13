d39c809c3861c5e1facfeb8051d29e2de19cfd86
ivil2021 <89508514326@mail.ru>
2021-09-14 01:01:07 +0300

    added materialize and some routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { useRoutes } from './routes';
 import 'materialize-css';
 
+console.log('123');
+
 function App() {
+    console.log('456');
+
+    const routes = useRoutes(false);
+    console.log('789');
+    console.log(routes);
     return (
-        <div className="container">
-            <h1>Hello</h1>
-        </div>
+        <Router>
+            <div className="container">{routes}</div>
+        </Router>
     );
 }
 
